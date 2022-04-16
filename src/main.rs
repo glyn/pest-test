0a63427a3a06c044dbe18d87c18d7cb65524b68b
Glyn Normington <glyn.normington@gmail.com>
2022-04-16 13:08:21 +0100

    CSV example from pest book

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "csv.pest"]
+pub struct CSVParser;
+
+fn main() {
+    println!("Hello, world!");
+}
