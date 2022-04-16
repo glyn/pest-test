3be519c85c3ab5d82577fc1b64491e1b1ab63a3c
Glyn Normington <glyn.normington@gmail.com>
2022-04-16 16:07:31 +0100

    Minimise the testcase

--- src/main.rs
+++ src/main.rs
@@ -2,46 +2,20 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-mod ast;
-use ast::*;
 use pest::Parser;
-use std::num::ParseIntError;
 
 #[derive(Parser)]
-#[grammar = "csv.pest"]
-pub struct CSVParser;
+#[grammar = "test.pest"]
+pub struct TestParser;
 
 fn main() {
-    let selector_rule = CSVParser::parse(Rule::selector, " $")
+    TestParser::parse(Rule::selector, " $")
         .map_err(|e| format!("{}", e))
         .map_err(|e| {
             println!("{}", e);
             e
         })
-        .expect("invalid parse result")
-        .nth(1)
-        .unwrap();
+        .expect("parse failure as expected");
 
-    let result = selector_rule
-        .into_inner()
-        .fold(Ok(Path::Root), |prev: Result<_, String>, r| {
-            match r.as_rule() {
-                Rule::matcher => Ok(Path::Sel(
-                    Box::new(prev?),
-                    parse_selector(r).map_err(|e| format!("{}", e))?,
-                )),
-                _ => panic!("invalid parse tree {:?}", r),
-            }
-        });
-
-    println!("result={:?}", result);
-}
-
-fn parse_selector(matcher_rule: pest::iterators::Pair<Rule>) -> Result<Selector, ParseIntError> {
-    let r = matcher_rule.into_inner().next().unwrap();
-
-    Ok(match r.as_rule() {
-        Rule::matcher => Selector::Matcher,
-        _ => panic!("invalid parse tree {:?}", r),
-    })
+    println!("Test failed - should have errored already");
 }
